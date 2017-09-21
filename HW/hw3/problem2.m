37bf861aaa574182e35795b22a6c056952ed0af3
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-09-20 23:32:00 -0700

    d

--- HW/hw3/problem2.m
+++ HW/hw3/problem2.m
@@ -1,6 +1,6 @@
 %%
 syms C w G R L
-syms Z0 gamma l
+syms Z0 g l
 
 Z1 = ((2/G)*(1/1i*w*C) / ((2/G) + (1/1i*w*C)));
 %Z1 = 2 / (2*1i*w*C + G);
@@ -11,16 +11,13 @@ Z2 = R + 1i*w*L;
 Z_11 = (Z1*(Z2 + Z3)) / (Z1 + Z2 + Z3);
 Z_12 = (Z1 * Z3) / (Z1 + Z2 + Z3);
 
-%Z_11_tline = Z0 * (1 / tanh(gamma*l));
-%Z_12_tline = Z0 * tanh(gamma*l);
+Z_11_tline = Z0 / ((g*l - (g*l)^3 / 3 + 2*(g*l)^5 / 15));
+Z_12_tline = Z0 / ((g*l + (g*l)^3 / 3*2 + (g*l)^5 / 5*4*3*2));
 
-Z_11_tline = Z0 * (1 / (gamma*l - (gamma*l)^3 / 3));
-Z_12_tline = Z0 * (gamma*l - (gamma*l)^3 / 3);
-
-gamma_RLGC_relation = gamma == sqrt((R + 1i*w*L) * (G + 1i*w*C));
+g_RLGC_relation = g == sqrt((R + 1i*w*L) * (G + 1i*w*C));
 Z0_LC_relation = Z0 == sqrt((R + 1i*w*L)/(G + 1i*w*C));
 
-sol = solve([Z_11 == Z_12_tline, Z_12 == Z_11_tline, gamma_RLGC_relation, Z0_LC_relation], [R, L, G, C]);
+sol = solve([Z_11 == Z_11_tline, Z_12 == Z_12_tline, g_RLGC_relation, Z0_LC_relation], [R, G, L, C], 'IgnoreAnalyticConstraints', true);
 
 %%
 limit(sol.C, l, 0)
